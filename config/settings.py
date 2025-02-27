b21dcab205dbdc513d1a5ed6cf813eeeacef55df
kyama/Pe <18105767+kyamadev@users.noreply.github.com>
2025-02-27 18:34:41 +0900

    Update token lifetime settings

--- config/settings.py
+++ config/settings.py
@@ -54,8 +54,8 @@ DEFAULT_FROM_EMAIL = 'noreply@example.com'
 
 from datetime import timedelta
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
 }
